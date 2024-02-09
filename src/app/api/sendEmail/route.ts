24b26ba35474d8f43a3ce82732da1c356de8ec32
llPornpilin <linglingppl@gmail.com>
2024-02-09 17:46:17 +0700

    auto send email

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -42,33 +42,3 @@ export async function POST(request: any) {
         return NextResponse.json({message: "Fail to send Email"}, {status: 500})
     }
 }
-
-
-cron.schedule('* * * * *', async() => {
-    console.log('Cron job started!')
-    console.log("CRON Active every 1 minutes !")
-    try {
-        await axios.post('http://localhost:3000/api/sendEmail', {
-            title: 'CRON SEND Email',
-            subject: 'Test Send Email By Cron 2 minutes ...',
-            message: 'send email successfully !!',
-        });
-
-        console.log('Email sent successfully');
-    }
-    catch(error) {
-        console.error('CRON Failed to send email:', error);
-    }
-})
-
-// ------------------------------------------------------------------------
-// const logMessage = () => {
-//     console.log("Cron job started!");
-//     console.log("w,j9hv'lj'g,]");
-// }
-
-// logMessage()
-
-// cron.schedule("* * * * *", logMessage);
-
-
--- src/cron/cron.js
+++ src/cron/cron.js
@@ -1,6 +1,19 @@
 const cron = require("node-cron");
+const axios = require('axios');
 
-cron.schedule("* * * * *", () => {
-  // Your task to be executed every minute goes here
-  console.log("Cron job executed every minute");
-});
+cron.schedule('* * * * *', async() => {
+  console.log('Cron job started!')
+  console.log("CRON Active every 1 minutes !")
+  try {
+      await axios.post('http://localhost:3000/api/sendEmail', {
+          title: 'CRON SEND Email',
+          subject: 'Test Send Email By Cron 2 minutes ...',
+          message: 'send email successfully !!',
+      });
+
+      console.log('Email sent successfully');
+  }
+  catch(error) {
+      console.error('CRON Failed to send email:', error);
+  }
+})

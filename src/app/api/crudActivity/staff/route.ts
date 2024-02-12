4f1740331ec3acc59c16340f45ee64906d93afda
Tadporn <88502892+Tadphorn@users.noreply.github.com>
2024-02-12 21:21:11 +0700

    crudActivity

--- src/app/api/crudActivity/staff/route.ts
+++ src/app/api/crudActivity/staff/route.ts
@@ -1,7 +1,63 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
 const tableName = 'StaffActivities'
 import iTryDynamoDB from "@/app/api/utils/dynamoDB";
 import { ITryActivity } from '@/app/utils/ManageActivityPage/activity';
+
+async function POST(req: ITryActivity) {
+    try {
+        console.log("start upload")
+        console.log("req", req)
+        const {
+            activityName,
+            imageUrl,
+            visibility,
+            activityDetails,
+            jobPositions,
+            openDate,
+            closeDate,
+            schedule,
+            phone,
+            email,
+            igLink,
+            facebookLink,
+            applyLink,
+            faq
+        } = req;
+
+        const activityId = await uuidv4();
+
+        const paramsDynamo = {
+            TableName: tableName,
+            Item: {
+                activityId: activityId,
+                activityName: activityName,
+                imageUrl: imageUrl,
+                jobPositions: jobPositions,
+                activityDetails: activityDetails,
+                visibility: visibility,
+                schedule: schedule,
+                openDate: openDate,
+                closeDate: closeDate,
+                phone: phone,
+                email: email,
+                applyLink: applyLink,
+                igLink: igLink,
+                facebookLink: facebookLink,
+                faq: faq
+            },
+        };
+
+        console.log("paramsDynamo", paramsDynamo)
+
+        const insertDynamo = await iTryDynamoDB.put(paramsDynamo).promise();
+        console.log("insertDynamo", insertDynamo);
+        return NextResponse.json("success")
+    } catch (error) {
+        console.log("error", error)
+        return NextResponse.json(error);
+    }
+}
 
 async function PUT(req: ITryActivity) {
     
@@ -13,6 +69,8 @@ async function PUT(req: ITryActivity) {
             visibility,
             activityDetails,
             jobPositions,
+            openDate,
+            closeDate,
             schedule,
             phone,
             email,
@@ -22,11 +80,13 @@ async function PUT(req: ITryActivity) {
             faq
         } = req;
         
-        let updateExpression = 'set imageUrl = :newImageUrl, activityName = :newActivityName, activityDetails = :newActivityDetails, jobPositions = :newJobPosition ,visibility = :newVisibility, schedule = :newSchedule, phone = :newPhone, email= :newEmail, applyLink= :newApplyLink, igLink = :newIgLink, facebookLink = :newFacebookLink, faq = :newFaq'
+        let updateExpression = 'set imageUrl = :newImageUrl, activityName = :newActivityName, activityDetails = :newActivityDetails, openDate = :newOpenDate, closeDate = :newCloseDate, jobPositions = :newJobPosition ,visibility = :newVisibility, schedule = :newSchedule, phone = :newPhone, email= :newEmail, applyLink= :newApplyLink, igLink = :newIgLink, facebookLink = :newFacebookLink, faq = :newFaq'
         let expressionAttributeValue = {
             ':newImageUrl': imageUrl,
             ':newActivityName': activityName,
             ':newActivityDetails': activityDetails,
+            ':newOpenDate': openDate,
+            ':newCloseDate': closeDate,
             ":newJobPositions": jobPositions,
             ':newVisibility': visibility,
             ':newSchedule': schedule,

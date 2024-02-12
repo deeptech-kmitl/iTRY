d98bb9cf4419f6fc25065f18d3ccbd993691e7d7
llPornpilin <linglingppl@gmail.com>
2024-02-12 20:02:19 +0700

    send email

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -5,11 +5,84 @@ import { CronJob } from "cron";
 import axios from 'axios'
 import { schedule } from '../create/staffActivity/mockupData';
 import { Console } from 'console';
+import { DynamoDB } from 'aws-sdk';
+import iTryDynamoDB from "@/app/api/utils/dynamoDB";
 
+const dynamodb = new DynamoDB.DocumentClient({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.REGION,
+})
 export async function POST(request: any) {
+
+    const paramsUsers = {
+        TableName: 'TestUsers', // TODO: Change to 'Users'
+    }
+    const paramsStaffAct = {
+        TableName: 'StaffActivities',
+    }
+    const paramsCamperAct = {
+        TableName: 'CamperActivities',
+    }
+
     try {
         console.log("_______ Begin ________")
         const {title, subject, message} = await request.json()
+
+        // ------------- GET Users From DynamoDB -------------
+        const usersData = await dynamodb.scan(paramsUsers).promise()
+        const usersEmail = (usersData.Items ?? []).map(item => item.email)
+        console.log("USERS -> ", usersEmail)
+
+        // ------------- GET Staff Activity From DynamoDB -------------
+        const staffActData = await dynamodb.scan(paramsStaffAct).promise()
+
+        // Tomorrow Date String
+        const tomorrowDate = new Date()
+        tomorrowDate.setDate(tomorrowDate.getDate() + 1)
+        const year = tomorrowDate.getFullYear();
+        const month = String(tomorrowDate.getMonth() + 1).padStart(2, '0');
+        const day = String(tomorrowDate.getDate()).padStart(2, '0');
+
+        const tomorrowDateString = `${year}-${month}-${day}`
+        console.log("Tomorrow Date String -- ", tomorrowDateString)
+        const testDateDB = (staffActData.Items ?? []).map(item => item.openDate)
+        console.log("TEST DATE -> ", testDateDB)
+
+        // <<<<<<<<<<<<<<<<<<<<<<<< SEND EMAIL, FILTER BY OPEN DATE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        // MAP necessary data to send email
+        const staffActivities = (staffActData.Items ?? [])
+        .filter(item => item.openDate == tomorrowDateString)
+        .map(item => ({
+            activityName: item.activityName,
+            activityDetails: item.activityDetails,
+            openDate: item.openDate,
+            activityLink: 'http://localhost:3000/api/activityById/staff/' + item.activityId,
+        }))
+        console.log("STAFF ACTIVITY -> ", staffActivities.map(activity => activity.activityName))
+
+        // ------------- GET Camper Activity From DynamoDB -------------
+        const camperActData = await dynamodb.scan(paramsCamperAct).promise()
+        const camperActivities = (camperActData.Items ?? [])
+        .filter(item => item.openDate == tomorrowDateString)
+        .map(item => ({
+            activityName: item.activityName,
+            activityDetails: item.activityDetails,
+            openDate: item.openDate,
+            activityLink: 'http://localhost:3000/api/activityById/camper/' + item.activityId,
+        }))
+        console.log("CAMPER ACTIVITY -> ", camperActivities.map(activity => activity.activityName))
+
+        const combinedActivities = [...staffActivities, ...camperActivities]
+        console.log("Combined Staff & Camper Activities -> ", combinedActivities)
+
+        // Check if there are staff activities scheduled for tomorrow
+        if (combinedActivities.length === 0) {
+            console.log("No any activities scheduled for tomorrow. Skipping email.");
+            return NextResponse.json({ message: "No any activities scheduled for tomorrow" }, { status: 200 });
+        }
+        
+        // ------------ SEND EMAIL --------------
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             host: 'smtp.gmail.com',
@@ -21,19 +94,98 @@ export async function POST(request: any) {
             }
         })
 
-        const mailOption = {
-            from: 'itrydpd@gmail.com',
-            to: 'llPornpilin@gmail.com',
-            subject: title,
-            html: `
-                <h3>Hello Camper !</h3>
-                <li> title: ${subject}</li>
-                <li> message: ${message}</li>
-            `
+        for (const activity of combinedActivities) {
+            const mailOption = {
+                from: 'itrydpd@gmail.com',
+                to: usersEmail.join(','),
+                subject: title,
+                html: `
+                <h3>Welcome to iTRY activity !</h3>
+                <p>Title: ${subject}</p>
+                <p>Message: ${message}</p>
+                    <h4>${activity.activityName}</h4>
+                    <p>Details: ${activity.activityDetails}</p>
+                    <p>Link: <a href="${activity.activityLink}">${activity.activityLink}</a></p>
+                `
+            }
+
+            await transporter.sendMail(mailOption)
+            console.log("_______ Finish SEND OPEN DATE EMAIL ________")
         }
 
-        await transporter.sendMail(mailOption)
-        console.log("_______ Finish ________")
+        // <<<<<<<<<<<<<<<<<< SEND EMAIL, FILTER BY SCHEDULE (FOLLOWED ACTIVITY) >>>>>>>>>>>>>>>>>>>>>>>>
+        const usersFollowedAct = (usersData.Items ?? []).filter(user => {
+            const followedActivities = user.followedActivityId || []
+            return followedActivities.some((activityId: string) => {
+                return (
+                    (camperActData.Items ?? []).some(item => 
+                        item.schedule.some((scheduleItem: any) => 
+                            scheduleItem.date == tomorrowDateString && item.activityId == activityId
+                        )
+                    ) || 
+                    (staffActData.Items ?? []).some(item => 
+                        item.schedule.some((scheduleItem: any) => 
+                            scheduleItem.date == tomorrowDateString && item.activityId == activityId
+                        )
+                    )
+                )
+            })
+        })
+
+        console.log("USER FOLLOW DATA _________",usersFollowedAct)
+        const emailFollowedAct = usersFollowedAct.map(user => user.email)
+        console.log("EMAIL THAT FOLLOWING ACTIVITIES -> ", emailFollowedAct)
+
+        if (emailFollowedAct.length === 0) {
+            console.log("No users to email. Skipping email.");
+            return NextResponse.json({ message: "No users to email" }, { status: 200 });
+        }
+
+        for (const email of emailFollowedAct) {
+            const user = (usersData.Items ?? []).find((user: any) => user.email === email)
+            console.log("USER ___ ", user)
+
+            const followedActivities = user?.followedActivityId || [];
+            console.log("FOLLOW ACTIVITY ___ ", followedActivities)
+
+            const acitivitySchedule = followedActivities.flatMap((activityId: any) => {
+                const activityDetails = (camperActData.Items ?? []).find(item => 
+                    item.activityId === activityId
+                ) || (staffActData.Items ?? []).find(item => 
+                    item.activityId === activityId
+                )
+                console.log("ACTIVITY DETAIL -- ", activityDetails)
+                
+                if (activityDetails && Array.isArray(activityDetails.schedule)) {
+                    return activityDetails.schedule.filter((scheduleItem: any) =>
+                        scheduleItem.date === tomorrowDateString
+                    );
+                }
+                return [];
+            })
+            console.log("SCHEDULE -> ", acitivitySchedule)
+
+            for (const scheduleItem of acitivitySchedule) {
+                const mailOption = {
+                    from: 'itrydpd@gmail.com',
+                    to: email,
+                    subject: title,
+                    html: `
+                    <h3>Hello Activity Follower !</h3>
+                    <p>Title: ${subject}</p>
+                    <p>Message: ${message}</p>
+                    <p>Activities scheduled for tomorrow:</p>
+                    <ul>
+                        <li>Date: ${scheduleItem.date}</li>
+                        <li>Detail: ${scheduleItem.details}</li>
+                    </ul>
+                    `
+                }
+                await transporter.sendMail(mailOption);
+                console.log("SCHEDULE Email sent to user: ", email)
+            }
+
+        }
 
         return NextResponse.json({message: "Email Sent Successfully"}, {status: 200})
     }
@@ -41,4 +193,6 @@ export async function POST(request: any) {
         console.error(error)
         return NextResponse.json({message: "Fail to send Email"}, {status: 500})
     }
+
+
 }
--- src/cron/cron.js
+++ src/cron/cron.js
@@ -1,19 +1,27 @@
 const cron = require("node-cron");
 const axios = require('axios');
+// const iTryDynamoDB = require("../app/api/utils/dynamoDB") // call DynamoDB new Version (now can not use)
 
-cron.schedule('* * * * *', async() => {
-  console.log('Cron job started!')
-  console.log("CRON Active every 1 minutes !")
-  try {
-      await axios.post('http://localhost:3000/api/sendEmail', {
-          title: 'CRON SEND Email',
-          subject: 'Test Send Email By Cron 2 minutes ...',
-          message: 'send email successfully !!',
-      });
+// ----------------- For Test call DynamoDB last Version ----------------------------
+const AWS = require('aws-sdk')
+const { DynamoDB } = AWS
+require('dotenv').config()
+// const { NextApiRequest, NextApiResponse } = require('next')
 
-      console.log('Email sent successfully');
-  }
-  catch(error) {
-      console.error('CRON Failed to send email:', error);
-  }
-})
+
+// const dynamodb = new DynamoDB.DocumentClient({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.REGION,
+// })
+// ----------------------------------------------------------------------------------
+
+// cron.schedule('* * * * *', async() => {
+//   console.log('----- Cron job started! -----')
+
+//     await axios.post('http://localhost:3000/api/sendEmail', {
+//         title: 'CRON SEND Email To everyone',
+//         subject: 'Welcome to iTRY',
+//         message: 'send email to everyone successfully !!',
+//     });
+// })

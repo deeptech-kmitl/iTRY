251ba962c1f640895f9d4e6d38903313f4d00b0e
EntityKunG <64070149@KMITL.AC.TH>
2024-02-14 19:56:41 +0700

    post

--- src/app/api/banner/uploadImage/route.ts
+++ src/app/api/banner/uploadImage/route.ts
@@ -1,20 +1,35 @@
 import { NextResponse } from "next/server";
 import { uploadFileToS3 } from "../../create/staffActivity/route";
+import { v4 as uuidv4 } from "uuid";
+import iTryDynamoDB from "../../utils/dynamoDB";
 
 export async function POST(request: any) {
     try {
       const formData = await request.formData();
-      const file = formData.get("file") as File;
+      const file = formData.get("file");
   
       if (!file) {
         return NextResponse.json({ error: "File is required." }, { status: 400 });
       }
-  
+      // S3
       const buffer = Buffer.from(await file.arrayBuffer());
-      const fileName = await uploadFileToS3(buffer, file.name);
+      const responseS3 = await uploadFileToS3(buffer);
       console.log("yayy");
-      return NextResponse.json({ success: true, fileName });
+  
+      //dynamodb
+      let myuuid = uuidv4();
+      const paramsDynamo = {
+        TableName: "Banner",
+        Item: {
+          bannerId: myuuid,
+          bannerUrl: responseS3, // url image in S3
+        },
+      };
+      // Insert data into DynamoDB
+      const insertDynamo = await iTryDynamoDB.put(paramsDynamo).promise();
+      return NextResponse.json({ success: true });
     } catch (error) {
       return NextResponse.json({ error });
     }
   }
+  

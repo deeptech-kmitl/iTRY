48bdf7013d90e8d9bb8ed5a68078a4af69725d7d
63070054 <63070054@kmitl.ac.th>
2024-02-19 22:17:44 +0700

    region

--- src/app/api/utils/dynamoDB.ts
+++ src/app/api/utils/dynamoDB.ts
@@ -4,9 +4,9 @@ import { DynamoDB } from 'aws-sdk';
 const iTryDynamoDB = new DynamoDB.DocumentClient({
   accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-  region: process.env.NEXT_PUBLIC_REGION,
+  region: process.env.NEXT_PUBLIC_REGION || process.env.REGION,
 });
 
-console.log("process.env.NEXT_PUBLIC_REGION,", process.env.NEXT_PUBLIC_REGION)
+console.log("REGION,", process.env.NEXT_PUBLIC_REGION || process.env.REGION)
 
 export default iTryDynamoDB;

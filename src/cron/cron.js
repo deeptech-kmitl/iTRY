ec7ae6d3d8f4ae59d6fc8a44a1cfda76489d1a15
llPornpilin <linglingppl@gmail.com>
2024-03-11 16:53:24 +0700

    set cron every 1 minute

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk')
 const { DynamoDB } = AWS
 require('dotenv').config()
 
-cron.schedule('0 9 * * *', async() => {
+cron.schedule('* * * * *', async() => {
   console.log('----- Cron job started! -----')
 
   await axios.post('http://localhost:3000/api/sendEmail/byCron');

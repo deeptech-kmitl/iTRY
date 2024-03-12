9e37d452720fbdfdf2e38ee924db8302be131da2
Tadporn <88502892+Tadphorn@users.noreply.github.com>
2024-03-12 14:27:12 +0700

    responsive cardposition

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 # Specify the base image
 FROM node:20
 
-ENV NEXTAUTH_URL=https://itryweb.com
-ENV NEXTAUTH_URL_INTERNAL=https://itryweb.com
-
 # Set the working directory
 WORKDIR /app
 
@@ -11,16 +8,16 @@ WORKDIR /app
 COPY package.json yarn.lock* ./
 
 # Install dependencies
-RUN NODE_ENV=development npm i
+RUN yarn install
 
 # Copy the rest of the application code
 COPY . .
 
-RUN npm run build
+# RUN npm run build
 
 EXPOSE 3000
 
 
 # Start the Next.js app
-CMD ["npm", "dev"]
+CMD ["yarn", "dev"]
 
--- src/app/components/CardPosition.tsx
+++ src/app/components/CardPosition.tsx
@@ -2,17 +2,18 @@
 
 import { JobPositionsActivity } from "../utils/ManageActivityPage/activity";
 
-export default function CardPosition({amount, name}: JobPositionsActivity) {
-    return (
-        <div
-            className="card w-40 h-56 bg-base-100 border-solid border-2 border-neonBlue rounded-md">
-            <div className="py-2 h-[50%] bg-lightBlue flex justify-center text-center">
-                <h5 className="card-title justify-center text-xl text-black">{name}</h5>
-            </div>
-            <div className="py-2 h-[50%] bg-[#2B79A4] flex flex-col justify-center items-center">
-                <h5 className="text-xs">จำนวน</h5>
-                <h5 className="card-title text-2xl pt-2">{amount}</h5>
-            </div>
-        </div>
-    )
+export default function CardPosition({ amount, name }: JobPositionsActivity) {
+  return (
+    <div className="card w-full card-position bg-base-100 border-solid border-2 border-neonBlue rounded-md">
+      <div className="py-2 h-[50%] bg-lightBlue flex justify-center text-center">
+        <h5 className="card-title justify-center text-sm xl:text-xl text-black">
+          {name}
+        </h5>
+      </div>
+      <div className="py-2 h-[50%] bg-[#2B79A4] flex flex-col justify-center items-center">
+        <h5 className="text-xs">จำนวน</h5>
+        <h5 className="card-title text-2xl pt-2">{amount}</h5>
+      </div>
+    </div>
+  );
 }

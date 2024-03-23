c2350d9b840be422c21318df62af3c62629db525
llPornpilin <linglingppl@gmail.com>
2024-03-23 11:53:07 +0700

    update

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -7,5 +7,5 @@ require('dotenv').config()
 cron.schedule('* * * * *', async() => {
   console.log('----- Cron job started! -----')
 
-  await axios.post(`https://itryweb.com/api/sendEmail/byCron`);
+  await axios.post(`http://localhost:3000/api/sendEmail/byCron`);
 })

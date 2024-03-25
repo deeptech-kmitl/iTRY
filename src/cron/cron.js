95875f75afdb0f4185dd39dd0ab686452a97ef30
llPornpilin <linglingppl@gmail.com>
2024-03-25 22:48:52 +0700

    Update cron.js

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk')
 const { DynamoDB } = AWS
 require('dotenv').config()
 
-cron.schedule('* * * * *', async() => {
+cron.schedule('0 9 * * *', async() => {
   console.log('----- Cron job started! -----')
 
   await axios.post(`http://localhost:3000/api/sendEmail/byCron`);

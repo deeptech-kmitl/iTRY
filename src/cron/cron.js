aaf0ddf73eb58d095afefc9f7a3c7e58a6083760
63070054 <63070054@kmitl.ac.th>
2024-03-25 23:00:15 +0700

    make cron use process env on Cron

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -1,11 +1,12 @@
 const cron = require("node-cron");
 const axios = require('axios');
 const AWS = require('aws-sdk')
-const { DynamoDB } = AWS
 require('dotenv').config()
 
 cron.schedule('* * * * *', async() => {
   console.log('----- Cron job started! -----')
-
-  await axios.post(`https://itryweb.com/api/sendEmail/byCron`);
+  const domain = process.env.NEXTAUTH_URL || "http://localhost:3000"
+  console.log('on : ' + domain)
+  
+  await axios.post(`${domain}/api/sendEmail/byCron`);
 })

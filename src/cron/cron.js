fa4e8279ccd5e1154ec0644d9474375a7ffc2384
63070054 <63070054@kmitl.ac.th>
2024-03-25 23:31:18 +0700

    cron back to localhost

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -5,8 +5,6 @@ require('dotenv').config()
 
 cron.schedule('0 9 * * *', async() => {
   console.log('----- Cron job started! -----')
-  const domain = process.env.NEXTAUTH_URL || "http://localhost:3000"
-  console.log('on : ' + domain)
-  
-  await axios.post(`${domain}/api/sendEmail/byCron`);
+
+  await axios.post(`http://localhost:3000/api/sendEmail/byCron`);
 })
